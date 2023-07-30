2444c8ec9ca010fd46e79ccbe5bca516b6736fe2
Maijuddin Mallick <124431155+Maijuddin@users.noreply.github.com>
2023-07-31 00:37:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+window.onload = function() {
+  // Get references to the dropdown list and the button
+  const colorSelect = document.getElementById('colorSelect');
+  const removeButton = document.querySelector('input[type="button"]');
+
+  // Function to handle the removal of the selected color
+  function removeColor() {
+    const selectedIndex = colorSelect.selectedIndex;
+    if (selectedIndex !== -1) {
+      colorSelect.remove(selectedIndex);
+    }
+  }
+
+  // Attach the removeColor function to the button's click event
+  removeButton.addEventListener('click', removeColor);
+};
